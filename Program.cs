2b8daff01e741e525027db10c38445a16269e617
David Smith <kittoes0124@byteterrace.com>
2021-11-04 11:10:33 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+Console.WriteLine(MyClass.magic(1));
+Console.WriteLine(MyClass.magic(2));
+Console.WriteLine(MyClass.magic(3));
+Console.ReadLine();
+
+static class MyClass
+{
+    public static int magic(uint x, uint y) {
+        return (BitOperations.PopCount(x) + BitOperations.PopCount(y));
+    }
+    public static int magic(uint x) {
+        return magic(x, x);
+    }
+}
